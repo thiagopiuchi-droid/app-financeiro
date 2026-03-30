268b47379cc6d11b76e71d6fd5b8c6c611959d55
thiagopiuchi-droid <thiagopiuchi@icloud.com>
2026-03-30 20:29:37 -0300

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAji6ROLmn3BkD92gZnQOMjRDgMX1hFd74",
-  authDomain: "app-financeiro-7043f.firebaseapp.com",
-  projectId: "app-financeiro-7043f",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
