46b1d2b88b330e14e62c9b30b462dcec3c29e6ce
thiagopiuchi-droid <thiagopiuchi@icloud.com>
2026-03-30 19:48:05 -0300

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-// COLE SEU FIREBASE AQUI
-const firebaseConfig = {
- apiKey: "SUA_KEY",
- authDomain: "SEU_DOMINIO",
- projectId: "SEU_ID"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
