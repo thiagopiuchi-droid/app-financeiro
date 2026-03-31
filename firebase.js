35c0439ac83ecb1592a330a839e261f8f565a50e
thiagopiuchi-droid <thiagopiuchi@icloud.com>
2026-03-31 20:12:13 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+apiKey: "AIzaSyAji6ROLmn3BkD92gZnQOMjRDgMX1hFd74",
+authDomain: "app-financeiro-7043f.firebaseapp.com",
+projectId: "app-financeiro-7043f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
